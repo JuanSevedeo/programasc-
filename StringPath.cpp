40d770366500f058ae82e9c9637594e3b45d78a0
JuanSevedeo <juanidkssj@gmail.com>
2025-06-03 20:59:13 -0600

    StringPath.cpp

--- StringPath.cpp
+++ StringPath.cpp
@@ -2,48 +2,42 @@
 #include <string>
 #include <stack>
 #include <map>
-
 using namespace std;
 
-main(){
-	string texto;
-	stack<char> lifo;
-	string inicio = "S1";
-	
-	map<string, map<char, string> > caminos;
-	
-	caminos["S1"]['a'] = "S2";
-	
-	caminos["S2"]['a'] = "S2";
-	caminos["S2"]['c'] = "S4";
-	caminos["S2"]['b'] = "S1";
-		
-	caminos["S3"]['b'] = "S4";
-	caminos["S3"]['a'] = "S1";
-	
-	caminos["S4"]['d'] = "S3";
-	
-	caminos["S4"]['a'] = "S5";
-	
-	cout << "Ingresa el texto: ";
-	cin >> texto;
-	
-	for(size_t i = 0; i < texto.length(); ++i){
-		lifo.push(texto[i]);
-	}
-	
-	while(!lifo.empty()){
-		char letra = lifo.top();
-		lifo.pop();
-		
-		if(caminos[inicio].count(letra)){
-			inicio = caminos[inicio][letra];
-			cout << letra << " avanzo a: " << inicio << endl;
-		}
-		else{
-			cout << letra << " no puede avanzar." << endl;
-			cout << "El texto se queda en: " << inicio;
-			break;
-		}
-	}
+int main() {
+    string texto;
+    stack<char> lifo;
+    string estadoActual = "S1";
+    map<string, map<char, string>> transiciones = {
+        {"S1", {{'a', "S2"}}},
+        {"S2", {{'a', "S2"}, {'c', "S4"}, {'b', "S1"}}},
+        {"S3", {{'b', "S4"}, {'a', "S1"}}},
+        {"S4", {{'d', "S3"}, {'a', "S5"}}}
+    };
+
+    cout << "Ingresa el texto: ";
+    cin >> texto;
+
+    // Agregar caracteres al stack
+    for (char c : texto) {
+        lifo.push(c);
+    }
+
+    // Procesar el texto
+    while (!lifo.empty()) {
+        char simbolo = lifo.top();
+        lifo.pop();
+
+        if (transiciones[estadoActual].count(simbolo)) {
+            string estadoSiguiente = transiciones[estadoActual][simbolo];
+            cout << simbolo << " avanzó a: " << estadoSiguiente << endl;
+            estadoActual = estadoSiguiente;
+        } else {
+            cout << simbolo << " no puede avanzar." << endl;
+            cout << "El texto se queda en: " << estadoActual << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
