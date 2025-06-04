82c351e809b088f77b6cb1b1bf8b41bfe3adb110
JuanSevedeo <juanidkssj@gmail.com>
2025-06-03 20:52:42 -0600

    Area Circulo.cpp
    el programa nos da el area y el perimetro de un circulo todo el programa es sin clases

--- Area Circulo.cpp
+++ Area Circulo.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
-
 using namespace std;
-float pi = 3.1416;
 
-float calcularArea(float radio){
-	return pi * (radio * radio);
+const float PI = 3.1416;
+
+// Función para calcular el área de un círculo
+float calcularArea(float radio) {
+    return PI * (radio * radio);
 }
 
-float calcularPerimetro(float radio){
-	return 2 * pi * radio;
+// Función para calcular el perímetro de un círculo
+float calcularPerimetro(float radio) {
+    return 2 * PI * radio;
 }
 
-main(){
-	char opcion;
-	float radio;
-	
-	cout << "Ingresa el radio del circulo: ";
-	cin >> radio;
-	
-	while(opcion != 's' || opcion != 'S'){
-		cout << endl << "Selecciona que deseas calcular: " << endl << "(A)Area" << endl << "(P)Perimetro" << endl << "(S)Salir" << endl;
-		cin >> opcion;
-	
-		if(opcion == 'a' || opcion == 'A'){
-			cout << "El area del circulo es: " << calcularArea(radio) << endl;
-		}
-		else if(opcion == 'p' || opcion == 'P'){
-			cout << "El perimetro del circulo es: " << calcularPerimetro(radio) << endl;
-		}
-		else if(opcion == 's' || opcion == 'S'){
-			break;
-		}
-	}
+int main() {
+    float radio;
+    char opcion;
+
+    cout << "Ingresa el radio del círculo: ";
+    cin >> radio;
+
+    while (true) {
+        cout << endl << "Selecciona qué deseas calcular: " << endl;
+        cout << "(A) Área" << endl;
+        cout << "(P) Perímetro" << endl;
+        cout << "(S) Salir" << endl;
+        cin >> opcion;
+
+        switch (opcion) {
+            case 'a':
+            case 'A':
+                cout << "El área del círculo es: " << calcularArea(radio) << endl;
+                break;
+            case 'p':
+            case 'P':
+                cout << "El perímetro del círculo es: " << calcularPerimetro(radio) << endl;
+                break;
+            case 's':
+            case 'S':
+                cout << "Saliendo del programa..." << endl;
+                return 0;
+            default:
+                cout << "Opción inválida. Por favor, selecciona una opción válida." << endl;
+        }
+    }
+
+    return 0;
 }
