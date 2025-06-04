675700d5b8e80dc8808f79a2a57acf9ce03de4a2
JuanSevedeo <juanidkssj@gmail.com>
2025-06-03 20:48:21 -0600

    Animales class.cpp
    clasificamos diferentes animales dependiendo su forma de vida y les hicimos una pequeña descripcion de estos mismos

--- Animales class.cpp
+++ Animales class.cpp
@@ -1,75 +1,100 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class Animal{
-	public:
-		string animal, color;
-		int ojos, colas, patas;
-		
-		void ImprimirDatos(){
-			cout << "El " << animal << " es de color " << color << endl;
-			cout << "El " << animal << " tiene " << ojos << " ojos" << endl;
-			cout << "El " << animal << " tiene " << colas << " cola" << endl;
-			cout << "El " << animal << " tiene " << patas << " patas" << endl;
-		}
+// Clase base Animal
+class Animal {
+protected:
+    string nombre;
+    string color;
+    int ojos;
+    int colas;
+    int patas;
+
+public:
+    // Constructor
+    Animal(string nombre, string color, int ojos, int colas, int patas)
+        : nombre(nombre), color(color), ojos(ojos), colas(colas), patas(patas) {}
+
+    // Método para imprimir datos del animal
+    void ImprimirDatos() {
+        cout << "El " << nombre << " es de color " << color << endl;
+        cout << "El " << nombre << " tiene " << ojos << " ojos" << endl;
+        cout << "El " << nombre << " tiene " << colas << " cola" << endl;
+        cout << "El " << nombre << " tiene " << patas << " patas" << endl;
+    }
+
+    // Método virtual para imprimir métodos específicos
+    virtual void ImprimirMetodosEspecificos() = 0;
 };
 
-class Mamifero : public Animal{
-	public: 
-	
-		Crianza(){
-			cout << "El " << animal << " nace de su madre" << endl;
-		}
-		
-		AlimentacionCrias(){
-			cout << "El " << animal << " alimenta a sus crias con leche materna" << endl;
-		}
-		
-		Pelaje(){
-			cout << "El " << animal << " tiene pelaje" << endl;
-		}
-		
-		ImprimirMetodosEspecificos(){
-			Crianza();
-			AlimentacionCrias();
-			Pelaje();
-		}
+// Clase Mamifero que hereda de Animal
+class Mamifero : public Animal {
+public:
+    // Constructor
+    Mamifero(string nombre, string color, int ojos, int colas, int patas)
+        : Animal(nombre, color, ojos, colas, patas) {}
+
+    // Método para imprimir características de mamífero
+    void Crianza() {
+        cout << "El " << nombre << " nace de su madre" << endl;
+    }
+
+    void AlimentacionCrias() {
+        cout << "El " << nombre << " alimenta a sus crías con leche materna" << endl;
+    }
+
+    void Pelaje() {
+        cout << "El " << nombre << " tiene pelaje" << endl;
+    }
+
+    // Implementación del método virtual
+    void ImprimirMetodosEspecificos() override {
+        Crianza();
+        AlimentacionCrias();
+        Pelaje();
+    }
 };
 
-class Oviparo : public Animal{
-	public: 
-		Crianza(){
-			cout << "El " << animal << " nace de un huevo" << endl;
-		}
-		
-		AlimentacionCrias(){
-			cout << "El " << animal << " se alimenta por su cuenta al nacer" << endl;
-		}
-		
-		Pelaje(){
-			cout << "El " << animal << " no tiene pelaje" << endl;
-		}
-		
-		ImprimirMetodosEspecificos(){
-			Crianza();
-			AlimentacionCrias();
-			Pelaje();
-		}
+// Clase Oviparo que hereda de Animal
+class Oviparo : public Animal {
+public:
+    // Constructor
+    Oviparo(string nombre, string color, int ojos, int colas, int patas)
+        : Animal(nombre, color, ojos, colas, patas) {}
+
+    // Método para imprimir características de ovíparo
+    void Crianza() {
+        cout << "El " << nombre << " nace de un huevo" << endl;
+    }
+
+    void AlimentacionCrias() {
+        cout << "El " << nombre << " se alimenta por su cuenta al nacer" << endl;
+    }
+
+    void Pelaje() {
+        cout << "El " << nombre << " no tiene pelaje" << endl;
+    }
+
+    // Implementación del método virtual
+    void ImprimirMetodosEspecificos() override {
+        Crianza();
+        AlimentacionCrias();
+        Pelaje();
+    }
 };
 
-main(){
-	Mamifero perro;
-	Oviparo pinguino;
+int main() {
+    // Crear objetos de las clases Mamifero y Oviparo
+    Mamifero perro("perro", "Café", 2, 1, 4);
+    Oviparo pinguino("pingüino", "Blanco y Negro", 2, 1, 2);
 
-	perro.animal = "perro", perro.color = "Cafe", perro.ojos = 2, perro.colas = 1, perro.patas = 4;
-	perro.ImprimirDatos();
-	perro.ImprimirMetodosEspecificos();
-	
-	cout << endl;
-	
-	pinguino.animal = "pinguino", pinguino.color = "Blanco y Negro", pinguino.ojos = 2, pinguino.colas = 1, pinguino.patas = 2;
-	pinguino.ImprimirDatos();
-	pinguino.ImprimirMetodosEspecificos();
+    // Imprimir datos y métodos específicos de cada animal
+    perro.ImprimirDatos();
+    perro.ImprimirMetodosEspecificos();
+    cout << endl;
+    pinguino.ImprimirDatos();
+    pinguino.ImprimirMetodosEspecificos();
+
+    return 0;
 }
